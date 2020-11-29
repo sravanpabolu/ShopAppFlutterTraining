bc508df219f0f9c0e62e4356a25f757c115945c8
Sravan <sravanpabolu@gmail.com>
2020-11-30 00:49:43 +0530

    198. Local State vs App-wide State - part 1

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app_training/providers/products.dart';
 import 'package:shop_app_training/widgets/products_grid.dart';
 
+enum FilterOptions {
+  Favourites,
+  All,
+}
+
 class ProductsOverviewScreen extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
+    final productsContainer = Provider.of<Products>(context, listen: false);
+    
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FilterOptions value) => {
+              if (value == FilterOptions.Favourites) {
+                productsContainer.showFavoritesOnly()
+              } else {
+                productsContainer.showAll()
+              }
+            },
+            icon: Icon(
+              Icons.more_vert,
+            ),
+            itemBuilder: (context) => [
+              PopupMenuItem(child: Text('Only Favorites'), value: FilterOptions.Favourites),
+              PopupMenuItem(child: Text('Show All'), value: FilterOptions.All),
+            ],
+          )
+        ],
       ),
       body: ProductsGrid(),
     );
   }
 }
-
-
