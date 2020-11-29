66ca9349e3fccea05fec3b1fccf07ccf6a65121a
Sravan <sravanpabolu@gmail.com>
2020-11-30 01:06:48 +0530

    199. Addind shopping cart data

---
+++ lib/providers/cart.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem({
+    @required this.id,
+    @required this.title,
+    @required this.quantity,
+    @required this.price,
+  });
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return {...items};
+  }
+
+  void addItem(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
+      //change qty
+      _items.update(
+          productId,
+          (existingItem) => CartItem(
+                id: existingItem.id,
+                title: existingItem.title,
+                price: existingItem.price,
+                quantity: existingItem.quantity + 1,
+              ));
+    } else {
+      _items.putIfAbsent(
+          productId,
+          () => new CartItem(
+                id: DateTime.now().toString(),
+                title: title,
+                price: price,
+                quantity: 1,
+              ));
+    }
+  }
+}
