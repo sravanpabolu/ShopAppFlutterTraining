2bb8be15267c8d2c36f72be53926785bd7fb04f9
Sravan <sravanpabolu@gmail.com>
2020-12-22 23:47:43 +0530

    223. Image Input & Image Preview

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -10,12 +10,29 @@ class EditProductScreen extends StatefulWidget {
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
+  final _imageURLFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
+
+  @override
+  void initState() {
+    _imageURLFocusNode.addListener(_updateImageUrl);
+    super.initState();
+  }
 
   @override
   void dispose() {
     _priceFocusNode.dispose();
     _descriptionFocusNode.dispose();
+    _imageUrlController.dispose();
+    _imageURLFocusNode.removeListener(_updateImageUrl);
+    _imageURLFocusNode.dispose();
     super.dispose();
+  }
+
+  void _updateImageUrl() {
+    if (!_imageURLFocusNode.hasFocus) {
+      setState(() {});
+    }
   }
 
   @override
@@ -49,6 +66,37 @@ class _EditProductScreenState extends State<EditProductScreen> {
               maxLines: 3,
               keyboardType: TextInputType.multiline,
             ),
+            Row(crossAxisAlignment: CrossAxisAlignment.end, children: [
+              Container(
+                width: 100,
+                height: 100,
+                margin: EdgeInsets.only(
+                  top: 8,
+                  right: 10,
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 1,
+                    color: Colors.grey,
+                  ),
+                ),
+                child: _imageUrlController.text.isEmpty
+                    ? Text('Enter a URL')
+                    : FittedBox(
+                        fit: BoxFit.cover,
+                        child: Image.network(_imageUrlController.text),
+                      ),
+              ),
+              Expanded(
+                child: TextFormField(
+                  decoration: InputDecoration(labelText: 'Image URL'),
+                  keyboardType: TextInputType.url,
+                  textInputAction: TextInputAction.done,
+                  controller: _imageUrlController,
+                  focusNode: _imageURLFocusNode,
+                ),
+              ),
+            ]),
           ],
         )),
       ),
