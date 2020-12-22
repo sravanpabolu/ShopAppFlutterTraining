c172d9fc7e376f42383fac4a238ac17c961bba26
Sravan <sravanpabolu@gmail.com>
2020-12-22 22:20:43 +0530

    217. "Edit Product" Screen & A Problem, 218. Using Forms & Working with Form Inputs

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:shop_app_training/providers/cart.dart';
 import 'package:shop_app_training/providers/orders.dart';
 import 'package:shop_app_training/providers/products.dart';
 import 'package:shop_app_training/screens/cart_screen.dart';
+import 'package:shop_app_training/screens/edit_product_screen.dart';
 import 'package:shop_app_training/screens/orders_screen.dart';
 import 'package:shop_app_training/screens/product_detail_screen.dart';
 import 'package:shop_app_training/screens/products_overview_screen.dart';
@@ -42,6 +43,7 @@ class MyApp extends StatelessWidget {
           CartScreen.routeName: (context) => CartScreen(),
           OrdersScreen.routeName: (context) => OrdersScreen(),
           UserProductsScreen.routeName: (context) => UserProductsScreen(),
+          EditProductScreen.routeName: (context) => EditProductScreen(),
         },
       ),
     );
--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app_training/providers/products.dart';
+import 'package:shop_app_training/screens/edit_product_screen.dart';
 import 'package:shop_app_training/widgets/app_drawer.dart';
 import 'package:shop_app_training/widgets/user_product_item.dart';
 
@@ -18,7 +19,9 @@ class UserProductsScreen extends StatelessWidget {
         actions: [
           IconButton(
             icon: const Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
           ),
         ],
       ),
