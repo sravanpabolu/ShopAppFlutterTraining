5ee1761c0e90afb928dbbd951f96fd8826c30580
Sravan <sravanpabolu@gmail.com>
2020-12-23 21:22:31 +0530

    247. Updating Data via PATCH Requests

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -88,11 +88,9 @@ class _EditProductScreenState extends State<EditProductScreen> {
     });
 
     if (_editedProduct.id != null) {
-      Provider.of<Products>(context, listen: false)
+      await Provider.of<Products>(context, listen: false)
           .updateProduct(_editedProduct.id, _editedProduct);
-      setState(() {
-        _isLoading = false;
-      });
+
       Navigator.of(context).pop();
     } else {
       try {
@@ -114,13 +112,17 @@ class _EditProductScreenState extends State<EditProductScreen> {
             ],
           ),
         );
-      } finally {
-        setState(() {
-          _isLoading = false;
-        });
-        Navigator.of(context).pop();
-      }
+      } 
+      // finally {
+      //   setState(() {
+      //     _isLoading = false;
+      //   });
+      //   Navigator.of(context).pop();
+      // }
     }
+    setState(() {
+      _isLoading = false;
+    });
   }
 
   @override
